8f224d55ecebf5ff9c0adc51c23572213b2aa457
attack204 <757394026@qq.com>
2021-08-01 19:50:33 +0800

    feat(all): 调整项目架构

---
+++ web-api/src/main/java/com/gaokao/webapi/manage/OrderController.java
@@ -0,0 +1,17 @@
+package com.gaokao.webapi.manage;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author attack204
+ * date:  2021/7/20
+ * email: 757394026@qq.com
+ * desc: 订单相关
+ */
+@Slf4j
+@RestController
+@RequestMapping("/xhr/v1/orders")
+public class OrderController {
+}
