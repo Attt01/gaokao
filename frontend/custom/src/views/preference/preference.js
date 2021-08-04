3487e92645f353f4d38f72fdf329fe247772361f
antong <at2201110101@163.com>
2021-08-04 20:35:25 +0800

    feat(all):

--- frontend/custom/src/views/preference/preference.js
+++ frontend/custom/src/views/preference/preference.js
@@ -2,13 +2,582 @@
 export default {
   data() {
     return {
+      tableData: [
+      {
+        num: '志愿1',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿2',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿3',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿4',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿5',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿6',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿7',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿8',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿9',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿10',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿11',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿12',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿13',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿14',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿15',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿16',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿17',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿18',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿19',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿20',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿21',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿22',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿23',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿24',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿25',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿26',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿27',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿28',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿29',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿30',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿31',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿32',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿33',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿34',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿35',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿36',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿37',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿38',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿39',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿40',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿41',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿42',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿43',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿44',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿45',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+
+      {
+        num: '志愿46',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿47',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿48',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿49',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿50',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }, 
+      {
+        num: '志愿51',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿52',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿53',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿54',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿55',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿56',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿57',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿58',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿59',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿60',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },{
+        num: '志愿61',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },{
+        num: '志愿62',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },{
+        num: '志愿63',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },{
+        num: '志愿64',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },{
+        num: '志愿65',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },{
+        num: '志愿66',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },{
+        num: '志愿67',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },{
+        num: '志愿68',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },{
+        num: '志愿69',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿70',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿71',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿72',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿73',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿74',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿75',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿76',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿77',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿78',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿79',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿80',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿81',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿82',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿83',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿84',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿85',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿86',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿87',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿88',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿89',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿90',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿91',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿92',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿93',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿94',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿95',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      },
+      {
+        num: '志愿96',
+        school: '',
+        profession: '',
+        add:<el-button round>添加</el-button>
+      }]
     }
   },
   methods: {
     initData() {
-    }
-  },
+    },
+    
   mounted() {
     this.initData()
-  }
-}
+  },
+}}
