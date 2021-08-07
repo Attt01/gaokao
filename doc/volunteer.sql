11bbbbec5b28eb11d9659976e6c4322fad5ed1d9
attack204 <757394026@qq.com>
2021-08-07 21:59:02 +0800

    feat: 增加了admin-api接口骨架，调整了一下项目架构

--- doc/volunteer.sql
+++ doc/volunteer.sql
@@ -2,12 +2,15 @@ drop table if exists `tb_user_volunteer`;
 CREATE TABLE `tb_user_volunteer`
 (
     id             bigint auto_increment not null comment '主键',
-    user_id        bigint  not null default 0 comment '用户Id',
-    form_id        int     not null default 0 comment '志愿表Id',
-    volunteer_list text comment '志愿id。1,2;4,66;表示第一个志愿的id是2，第4个志愿的id是66',
-    generated_type tinyint not null default 0 comment '生成类型，默认是手动生成',
-    is_current     tinyint not null default 0 comment '是否为当前表',
-    generated_time bigint  not null default 0 comment '生成时间',
+    user_id        bigint      not null default 0 comment '用户Id',
+    form_id        int         not null default 0 comment '志愿表Id',
+    name           varchar(20) not null default '' comment '志愿表名称',
+    score          int         not null default 0 comment '用户分数',
+    subject        varchar(10) not null default '' comment '用户的选课信息',
+    volunteer_list text comment '志愿id。[2, 0, 0, 66];表示第一个志愿的id是2，第4个志愿的id是66',
+    generated_type tinyint     not null default 0 comment '生成类型，默认是手动生成',
+    is_current     tinyint     not null default 0 comment '是否为当前表',
+    generated_time bigint      not null default 0 comment '生成时间',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4 comment '用户志愿表';
@@ -21,6 +24,6 @@ CREATE TABLE `tb_volunteer`
     major_plan_id  varchar(10) default '' comment '专业招生计划id',
     major_score_id varchar(40) default '' comment '专业分数id',
     code           varchar(10) default '' comment '招生代码',
-        PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4 comment '用户志愿表';
