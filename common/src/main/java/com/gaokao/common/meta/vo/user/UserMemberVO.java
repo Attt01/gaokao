cc1be0bca65bf9aa9a9eb900e0381ff9ceda3c9a
wyc-0705 <2667334889@qq.com>
2021-08-15 18:30:09 +0800

    feat:usermembervo

--- common/src/main/java/com/gaokao/common/meta/vo/user/UserMemberVO.java
+++ common/src/main/java/com/gaokao/common/meta/vo/user/UserMemberVO.java
@@ -1,11 +1,48 @@
 package com.gaokao.common.meta.vo.user;
 
 import com.gaokao.common.meta.po.UserMember;
+import lombok.Data;
 
 /**
- * @author attack204
- * date:  2021/8/5
- * email: 757394026@qq.com
+ * @author wyc
+ * 2021/8/15
  */
+@Data
 public class UserMemberVO extends UserMember {
+
+    private Long id;
+    /**
+     * 用户名
+     */
+    private String username;
+    /**
+     * 昵称
+     */
+    private String nickname;
+    /**
+     * vip
+     */
+    private boolean is_vip;
+    /**
+     * 分数
+     */
+    private Integer score;
+    /**
+     * 排名
+     */
+    private long province_rank;
+    /**
+     * 选课
+     */
+    private String subject;
+    /**
+     * 手机
+     */
+    private String phone;
+
+
+
+
+
+
 }
