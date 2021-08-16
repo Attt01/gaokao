f1294adabb3129712ed26fb8b88a14ab3d418e64
Elvira0902 <yuhuayu2.hi@163.com>
2021-08-16 15:16:17 +0800

    feat:个人资料页修改

--- frontend/custom/src/views/profile/profile.js
+++ frontend/custom/src/views/profile/profile.js
@@ -1,18 +1,37 @@
 import { updatePwd } from '@/api/login'
+import { getInfo } from '@/api/login'
 
 export default {
   data() {
     return {
       activeName: 'profile',
-      user: {}
+      user: {
+        username: '',
+        vipIsOrNot: '',
+        nickname: '',
+        score: '',
+        provinceRank: '',
+        subject: '',
+        phone: '',
+        email: ''
+      }
     }
   },
-  mounted() {
+  mounted: function() {
     this.init()
   },
   methods: {
     init() {
-      this.user = this.$store.state.user.profile
+      getInfo().then(res => {
+        this.user.username = res.data.username
+        this.user.vipIsOrNot = res.data.vipIsOrNot
+        this.user.nickname = res.data.nickname
+        this.user.score = res.data.score
+        this.user.provinceRank = res.data.provinceRank
+        this.user.subject = res.data.subject
+        this.user.phone = res.data.phone
+        this.user.email = res.data.email
+      })
       console.log(this.user)
     },
     handleClick(tab, event) {
