ef8edfd6c1848640c5c281acf2911f9a8cdc3c16
wyc-0705 <2667334889@qq.com>
2021-08-16 10:31:58 +0800

    feat:新增注册传入排名，选课，成绩

--- common/src/main/java/com/gaokao/common/meta/vo/user/RegParams.java
+++ common/src/main/java/com/gaokao/common/meta/vo/user/RegParams.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotBlank;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 /**
@@ -23,5 +25,19 @@ public class RegParams {
 
     @NotBlank(message = "验证码不合法")
     private String veryCode;
+
+    @NotNull(message = "昵称不能为空")
+    private String nickname;
+
+    @NotNull(message = "成绩不能为空")
+    @Max(message = "成绩必须小于760",value = 760)
+    @Min(message = "成绩必须大于0", value = 0)
+    private Integer score;
+
+    @NotNull(message = "选课不能为空")
+    private String subject;
+
+    @NotNull(message = "排名不能为空")
+    private Long province_rank;
 
 }
