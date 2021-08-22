608bdd0ca9699ac912348bc2e949e21eef682632
ERUIHNIYHBKBNF <839203521@qq.com>
2021-08-22 17:22:53 +0800

    feat: 添加志愿推荐页展示信息

--- frontend/custom/src/views/recommand/recommand.js
+++ frontend/custom/src/views/recommand/recommand.js
@@ -2,6 +2,14 @@ import {provinceAndCityDataPlus} from "element-china-area-data";
 
 export default {
   data() {
+    // const validatePlan = (rule, value, callback) => {
+    //   alert("qwq");
+    //   if (this.plan.num[0] + this.plan.num[1] + this.plan.num[2] != 96) {
+    //     callback(new Error('三个志愿总和应该是96'));
+    //   } else {
+    //     callback();
+    //   }
+    // }
     return {
       userInfo: {
         score: 0,
@@ -20,25 +28,74 @@ export default {
             value: 3,
             label: '软件工程'
           }]
-        }]
+        }],
+        selected: []
+      },
+      school: {
+        props: { multiple: true },
+        options: [{
+          value: 'daxuetese',
+          label: '大学特色',
+          children: [
+            {
+              value: 'jiubawu',
+              label: '985',
+            },
+            {
+              value: 'eryaoyao',
+              label: '211',
+            },
+            {
+              value: 'yiben',
+              label: '一本',
+            }
+          ]
+        },
+        {
+          value: 'banxuexingzhi',
+          label: '办学性质',
+          children: [
+            {
+              value: '1',
+              label: '普通批',
+            },
+            {
+              value: '2',
+              label: '中外合办',
+            },
+            {
+              value: '3',
+              label: '校企合办',
+            }
+          ]
+        },
+        {
+          value: 'daxueleixing',
+          label: '大学类型',
+          children: [
+            {
+              value: '1',
+              label: '综合',
+            },
+            {
+              value: '2',
+              label: '理工',
+            },
+            {
+              value: '3',
+              label: '师范',
+            }
+          ]
+        }],
+        selected: []
       },
       area: {
         props: { multiple: true },
         options: provinceAndCityDataPlus,
-        // isShowDialog: false,
-        // areas: ['qwq', 'owo', 'aaa'],
-        selected: [],
-        tmpSelected: []
-      },
-      areaRules: {
-        tmpSelected: [
-          { required: true, message: '请至少选择一项', trigger: 'blur' }
-        ]
+        selected: []
       },
       plan: {
-        isShowDialog: false,
-        num: [],
-        tmpnum: []
+        num: []
       },
       batch: {
         options: [{
@@ -50,7 +107,10 @@ export default {
         }],
         selected: ''
       },
-      isAutoRecommand: true
+      isAutoRecommand: true,
+      // valiRules: {
+      //   plan: [{ validator: validatePlan, trigger: 'blur' }]
+      // }
     }
   },
   methods: {
@@ -60,39 +120,14 @@ export default {
       //console.log(this.userInfo);
     },
     onSubmit() {
-      alert('submit!');
+      // this.$refs['favorite'].validate((valid) => {
+      //   if (valid) {
+      //     alert('submit!');
+      //   } else {
+      //     return false;
+      //   }
+      // })
     },
-    onConfirmArea() {
-      // area.isShowDialog=false
-      // alert("qwq");
-      this.$refs.areaSelect.validate((isValid) => {
-        //alert("qqq");
-        if (isValid) {
-          //alert("Qwq");
-          this.area.selected = this.area.tmpSelected;
-          this.area.isShowDialog = false;
-        } else {
-          return false;
-        }
-      });
-    },
-    onConfirmPlan() {
-      let tmpNum = this.plan.tmpnum.slice(0), sum = 0;
-      //console.log(tmpNum);
-      for (let index = 0; index < 3; index++) {
-        if (tmpNum[index] == undefined || tmpNum[index] == "") {
-          tmpNum[index] = 0;
-        }
-        sum += parseInt(tmpNum[index]);
-      }
-      if (sum != 96)
-      {
-        this.$message('三种志愿总和应该是96');
-      } else {
-        this.plan.num = tmpNum.slice(0);
-        this.plan.isShowDialog = false;
-      }
-    }
   },
   computed: {
     userInfoStr() {
