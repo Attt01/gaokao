4385d5fbf91b7c34c60c541cb78f8c311b69bb38
Elvira0902 <yuhuayu2.hi@163.com>
2021-09-11 16:20:50 +0800

    fix:我的志愿页面修改

--- frontend/custom/src/views/myPreferenceList/myPreferenceList.js
+++ frontend/custom/src/views/myPreferenceList/myPreferenceList.js
@@ -1,4 +1,4 @@
-import { getVolunteer, deleteVolunteer,changeCurrentForm, updateVolunteerName } from '../../api/volunteer'
+import { getVolunteer, deleteVolunteerForm, changeCurrentForm, updateVolunteerName } from '../../api/volunteer'
 import { getInfo } from '../../api/login'
 export default {
   data() {
@@ -8,12 +8,14 @@ export default {
         name: '',
         type: '',
         createtime: '',
-        iscurrent: ''
+        iscurrent: false,
+        editName: false
       }],
       userId: 0,
       activeIndex: 0,
       checkList: [],
-      editName: false
+      // editName: false,
+      active: {}
     }
   },
   methods: {
@@ -28,7 +30,8 @@ export default {
               name: item.name,
               type: item.generatedType ? '自动生成' : '手动生成',
               createtime: new Date(item.generatedTime).toLocaleDateString(),
-              iscurrent: item.current ? '是' : '否'
+              iscurrent: item.current ? '是' : '否',
+              editName: false
             }
             if (data[index].iscurrent === '是') {
               this.activeIndex = data[index].id
@@ -42,82 +45,109 @@ export default {
     setCurrentPreference(preferData) {
       const params = {
         newFormId: preferData.id,
-        preFormId: this.activeIndex,
-        userId: this.userId
+        preFormId: this.activeIndex
       }
-      changeCurrentForm(params).then(() => {
+      this.active = this.tableData.find(function(value) {
+        return value.id === params.preFormId
+      })
+      changeCurrentForm(params).then((res) => {
+        if (res.code === 200) {
+          this.$message({
+            message: '修改当前志愿表成功',
+            type: 'success',
+            duration: 700
+          })
+        }
+        // console.log(this.active)
+        const preIndex = this.tableData.indexOf(this.active)
         const newIndex = this.tableData.indexOf(preferData)
-        const preIndex = this.activeIndex
+        // console.log('pre:' + preIndex)
+        // console.log('new:' + newIndex)
         this.tableData[preIndex].iscurrent = '否'
-        this.activeIndex = newIndex
+        this.activeIndex = preferData.id
         this.tableData[newIndex].iscurrent = '是'
       })
     },
     deletePreference(preferData) {
-      // confirm事件在删除前确认一下
-      const index = this.tableData.indexOf(preferData)
+    //   console.log(preferData)
+      // 判断要删除的是否是当前使用的志愿
       if (preferData.iscurrent === '是') {
-        this.activeIndex = 0
+        this.$message({
+          message: '不能删除当前使用志愿！如要删除请修改当前志愿表！',
+          type: 'warning',
+          duration: 2500
+        })
+        return
       }
-      // 200if
-      this.tableData.splice(index, 1)
-      if (this.activeIndex === 0 && this.tableData.length) {
-        this.tableData[0].iscurrent = '是'
+      // confirm事件在删除前确认一下
+      this.$confirm('此操作将永久删除该志愿表, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        deleteVolunteerForm(preferData.id).then((res) => {
+          if (res.code === 200) {
+            this.$message({
+              message: '删除志愿表成功！',
+              type: 'success',
+              duration: 700
+            })
+            const index = this.tableData.indexOf(preferData)
+            if (preferData.iscurrent === '是') {
+              this.activeIndex = 0
+            }
+            // 200if
+            this.tableData.splice(index, 1)
+            if (this.activeIndex === 0 && this.tableData.length) {
+              this.tableData[0].iscurrent = '是'
+            }
+          } else {
+            this.$message({
+              message: '删除志愿表失败！',
+              type: 'error',
+              duration: 700
+            })
+          }
+        })
       }
-      console.log(preferData)
-      const params = {
-        formId: preferData.id,
-        userId: this.userId
-      }
-      // deleteVolunteer(params)
+      )
     },
     handleSelectionChange(val) {
       this.checkList = val
     },
-    editVolunteerName() {
-      const data = []
-      this.tableData.forEach((item, index) => {
-        if (item.name === '') {
+    editVolunteerName(preferData) {
+      preferData.editName = !preferData.editName
+      if (!preferData.editName) {
+        // 保存志愿表名称
+        if (preferData.name === '') {
           this.$message({
             message: '志愿名不能为空',
             type: 'error'
           })
+          window.location.reload()
           return
         }
-      })
-      if (this.editName === true) {
         // 提交修改志愿名
-        getVolunteer().then(volunteer => {
-          volunteer.data.forEach((item, index) => {
-            data[index] = {
-              id: item.id,
-              name: item.name
-            }
-            if (this.tableData[index].name !== data[index].name) {
-              const param = {
-                formId: data[index].id,
-                name: this.tableData[index].name
-              }
-              updateVolunteerName(param).then(res => {
-                if (res.code === 200) {
-                  this.$message({
-                    message: '修改志愿名成功！',
-                    type: 'success',
-                    duration: 700
-                  })
-                } else {
-                  this.$message({
-                    message: '修改志愿名失败',
-                    type: 'error',
-                    duration: 700
-                  })
-                }
-              })
-            }
-          })
+        const param = {
+          formId: preferData.id,
+          name: preferData.name
+        }
+        updateVolunteerName(param).then(res => {
+          if (res.code === 200) {
+            this.$message({
+              message: '修改志愿名成功！',
+              type: 'success',
+              duration: 700
+            })
+          } else {
+            this.$message({
+              message: '修改志愿名失败',
+              type: 'error',
+              duration: 700
+            })
+          }
         })
       }
-      this.editName = !this.editName
     }
   },
   mounted() {
