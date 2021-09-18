030b78c3758730f6b06594cb389a92820f0f0a38
Elvira0902 <yuhuayu2.hi@163.com>
2021-09-18 22:11:24 +0800

    fix:vip

--- frontend/custom/src/components/Vip/index.vue
+++ frontend/custom/src/components/Vip/index.vue
@@ -5,8 +5,24 @@
     width="400px"
     center
     :before-close="handleClose">
-    <img class="vip-code" :src="require('@/assets/img/avatar.gif')"/>
+    <img class="vip-code" :src="require('@/assets/img/QR_code_fake.jpg')"/>
     <div class="vip-word">微信支付</div>
+    <h2 class="vip-price">￥99.00</h2>
+    <div class="vip-privilege">
+      <div class="vip-title">VIP特权</div>
+      <div class="icon-container">
+        <i class="el-icon-unlock vip-icon"></i>
+        <i class="el-icon-document vip-icon"></i>
+        <i class="el-icon-edit vip-icon"></i>
+        <i class="el-icon-data-line vip-icon"></i>
+      </div>
+      <div class="text-container">
+        <div class="text">解锁更多院校</div>
+        <div class="text">一键智能填报</div>
+        <div class="text">测算录取概率</div>
+        <div class="text">生成志愿表</div>
+      </div>
+    </div>
     <span slot="footer" class="dialog-footer">
     <el-button @click="hideDialog">取 消</el-button>
     <el-button type="primary" @click="hideDialog">确 定</el-button>
@@ -47,12 +63,44 @@ export default {
 
 <style scoped>
 .vip-code{
-  margin-left: 105px;
+  margin-left: 100px;
   width: 150px;
   height: 150px;
 }
 .vip-word{
   text-align: center;
-  font-size: 20px;
+  font-size: 15px;
+}
+.icon-container{
+  display: flex;
+}
+.vip-title{
+  height: 50px;
+  font-size: 15px;
+  margin-left: 20px;
+  color: #409EFF;
+}
+.vip-icon{
+  flex:1;
+  font-size: 30px;
+  text-align: center;
+  color: #409EFF;
+}
+.text-container{
+  display: flex;
+}
+.text{
+  flex:1;
+  text-align: center;
+  font-size: 10px;
+  color: #409EFF;
+}
+.vip-privilege{
+  border: solid #73b5fa thin;
+  margin-top: 30px;
+  margin-bottom: 20px;
+}
+.vip-price{
+  text-align: center;
 }
 </style>
