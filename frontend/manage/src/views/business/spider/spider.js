78d22c60deb156e080ebbc23721463ba84d90630
zhangMingZhu <2670632519@qq.com>
2021-09-28 15:06:31 +0800

    fix: 修复日期格式化的问题

--- frontend/manage/src/views/business/spider/spider.js
+++ frontend/manage/src/views/business/spider/spider.js
@@ -59,28 +59,52 @@ export default {
         }
       })
     },
-    // 时间格式化
-    formatDate1: function (row) {
-      return date.formatDate.format(
-        new Date(row.startTime),
-        'yyyy-MM-dd hh:mm'
-      )
-    },
-    formatDate2: function (row) {
-      if (row.endTime === 0) {
-        return date.formatDate.format(
-          new Date(row.startTime),
-          'yyyy-MM-dd hh:mm'
-        )
-      } else if (row.endTime === -1) {
-        return "尚未完成"
-      } else {
-        return date.formatDate.format(
-          new Date(row.endTime),
-          'yyyy-MM-dd hh:mm'
-        )
+    // 日期格式化
+    parseTime(time, pattern) {
+      if (arguments.length === 0 || !time) {
+        return null
       }
+      if (time === -1) {
+        return "尚未完成"
+      }
+      const format = pattern || '{y}-{m}-{d} {h}:{i}:{s}'
+      let date
+      if (typeof time === 'object') {
+        date = time
+      } else {
+        if ((typeof time === 'string') && (/^[0-9]+$/.test(time))) {
+          time = parseInt(time)
+        } else if (typeof time === 'string') {
+          time = time.replace(new RegExp(/-/gm), '/');
+        }
+        if ((typeof time === 'number') && (time.toString().length === 10)) {
+          time = time * 1000
+        }
+        date = new Date(time)
+      }
+      const formatObj = {
+        y: date.getFullYear(),
+        m: date.getMonth() + 1,
+        d: date.getDate(),
+        h: date.getHours(),
+        i: date.getMinutes(),
+        s: date.getSeconds(),
+        a: date.getDay()
+      }
+      const time_str = format.replace(/{(y|m|d|h|i|s|a)+}/g, (result, key) => {
+        let value = formatObj[key]
+        // Note: getDay() returns 0 on Sunday
+        if (key === 'a') {
+          return ['日', '一', '二', '三', '四', '五', '六'][value]
+        }
+        if (result.length > 0 && value < 10) {
+          value = '0' + value
+        }
+        return value || 0
+      })
+      return time_str
     },
+
     // 分页需要的4个方法
     fetchNext() {
       this.listQuery.page = this.listQuery.page + 1
