895a752a54dafabc8c514094cd745cc7a9880d0f
chino <839203521@qq.com>
2021-10-20 09:31:20 +0800

    fix: 修复前端bug

--- frontend/manage/src/views/dashboard/dashboard.js
+++ frontend/manage/src/views/dashboard/dashboard.js
@@ -3,14 +3,8 @@ import { getOverview } from '@/api/dashboard/dashboard'
 export default {
   data() {
     return {
-      todayOrderNum: '暂无数据',
-      allOrderNum: '暂无数据',
-      todaySaleVolume: '暂无数据',
-      allSaleVolume: '暂无数据',
-      todayFavoriteServiceNum: '暂无数据',
-      allFavoriteServiceNum: '暂无数据',
-      todayFavoriteShopNum: '暂无数据',
-      allFavoriteShopNum: '暂无数据'
+      allRegisteredUser: '暂无数据',
+      allRegisteredVip: '暂无数据'
     }
   },
   methods: {
@@ -18,14 +12,8 @@ export default {
       getOverview().then(response => {
         const vo = response.data
         if (vo) {
-          this.todayOrderNum = vo.todayOrderNum
-          this.allOrderNum = vo.allOrderNum
-          this.todaySaleVolume = vo.todaySaleVolume
-          this.allSaleVolume = vo.allSaleVolume
-          this.todayFavoriteServiceNum = vo.todayFavoriteServiceNum
-          this.allFavoriteServiceNum = vo.allFavoriteServiceNum
-          this.todayFavoriteShopNum = vo.todayFavoriteShopNum
-          this.allFavoriteShopNum = vo.allFavoriteShopNum
+          this.allRegisteredUser = vo.allRegisteredUser
+          this.allRegisteredVip = vo.allRegisteredVip
         }
       })
     }
