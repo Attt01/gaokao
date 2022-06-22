c7dd75827962a322e413737ebcaaba7fb03f2004
liugaoji <757394026@qq.com>
2022-06-22 10:20:47 +0800

    feat: fix

--- web-api/src/main/java/com/gaokao/WebApiApplication.java
+++ web-api/src/main/java/com/gaokao/WebApiApplication.java
@@ -3,8 +3,10 @@ package com.gaokao;
 import com.github.xiaolyuh.cache.config.EnableLayeringCache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class WebApiApplication {
 
     public static void main(String[] args) {
